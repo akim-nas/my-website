504124091012374a5f13e4f04962e20ca66b235f
akim-nas <98978747+akim-nas@users.noreply.github.com>
2022-12-29 12:24:09 -0500

    added new pages, added methods, documentationjs

--- __init__.py
+++ __init__.py
@@ -2,9 +2,21 @@ from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
     return render_template("home.html")
+
+@app.route("/news", methods=['GET'])
+def news():
+    return render_template("news.html")
+
+@app.route("/proj", methods=['GET'])
+def proj():
+    return render_template("proj.html")
+
+@app.route("/support", methods=['GET'])
+def support():
+    return render_template("support.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,10 @@
-const changeHomeLink = () => document.getElementById("home").innerHTML = "you're already home";
-
+// Dark/light mode button
 const darkMode = () => {
     let element = document.body;
     element.classList.toggle("dark-mode");
 }
 
+// Auto dark/light mode
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     darkMode();
 }
